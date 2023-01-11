14f5834359bc495851787fa8ab40333ad9ed7b5d
So8oS <ahmadothman033@gmail.com>
2023-01-12 02:41:12 +0300

    resposiveness fix for everything (95%) - edited card

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -6,19 +6,19 @@ const Intro = () => {
     md:flex-row gap-12  '>
         <div className='flex flex-col '>
 
-            <span className='text-[1.75rem]'> Helloo, I&apos;m Ahmad </span>
+            <span className=' text-[1.5rem] md:text-[1.75rem]'> Helloo, I&apos;m Ahmad </span>
             <div className='flex flex-col animate-pulse'>
-                <span className='font-bold text-7xl  '>Front-end</span>
-                <span className='font-bold text-7xl  '>Developer</span>
+                <span className='text-6xl font-bold md:text-7xl  '>Front-end</span>
+                <span className='text-6xl font-bold md:text-7xl  '>Developer</span>
             </div> 
-            <span className='text-[1.75rem] animate-bounce '>woooooooo</span>
+            <span className=' animate-bounce text-[1.5rem] md:text-[1.75rem] '>woooooooo</span>
             <button 
             className=' w-40 h-14 font-Poppins  bg-darkYellow stripes border 
             hover:scale-110 transition-all duration-300 active:scale-75
             border-darkBrown  text-darkBrown font- shadow-2xl rounded-xl mt-6' >Resume</button>
         
         </div>     
-        <div className=" w-96 border-2 border-white rounded-circle" style={{ clipPath: 'polygon(50% 0%, 80% 10%, 100% 1%, 96% 70%, 80% 90%, 50% 100%, 20% 90%, 0% 70%, 0% 35%, 20% 10%)' }} >
+        <div className="  w-72 border-white md:w-[22rem] " style={{ clipPath: 'polygon(50% 0%, 80% 10%, 100% 1%, 96% 70%, 80% 90%, 50% 100%, 20% 90%, 0% 70%, 0% 35%, 20% 10%)' }} >
             <img className='' src="/pic.jpg" alt="ahmad" />
         </div>   
     </div>
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,25 +1,42 @@
 import React from 'react'
+import { AiFillGithub } from 'react-icons/ai';
 
 interface projectCardProps{
   name: string;
   image: string;
   description: string;
   status: string;
+  link : string;
+  github : string;
 }
 
 
 
 export const ProjectCard = ( props:projectCardProps) => {
   return (
-    <div className=' flex flex-col w-[23rem] h-96 rounded-md gap-3 hover:scale-110 transition-all duration-500  '>
+    <div className=' flex flex-col  w-[20rem] justify-center items-center  h-[25rem] rounded-md gap-3 
+     md:w-[23rem]
+     md:hover:scale-110 transition-all duration-500 
+     '>
         <div>
             <img className='rounded-md w-80 h-48' src={props.image} alt="" />
+            
         </div>
-            <span className=''>{`Status: ${props.status}`}</span>
-        <div className='flex flex-col '>
+
+        <span className=''>{`Status: ${props.status}`}</span>
+
+        <div className='flex flex-col items-center text-center h-10 mb-12'>
             <span className='font-bold text-lg '>{props.name}</span>
-            <span className=''>{props.description}</span>
+            <span className='w-[20.2rem] '>{props.description}</span>
         </div>
+
+        <div className='flex justify-center items-center gap-2'>
+          <a className='hover:text-gray-500 ' href={props.github} target='_blank' rel="noreferrer"><AiFillGithub className='cursor-pointer  ' /></a>
+          <a className='' href={props.link} >
+          <span className='font-extrabold hover:text-gray-500' >browse</span>
+        </a>
+        </div>
+        
     </div>
   )
 }
