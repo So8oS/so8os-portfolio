c1fe49aef30347d5eb4bd71b43c008987870dcd6
Ahmad Othman <62267611+So8oS@users.noreply.github.com>
2023-06-30 23:49:03 +0400

    Update projects.tsx

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -20,15 +20,6 @@ export const projects = [
         },
         {
             id : 2,
-            title :  "Le Khalil Le Cookies",
-            description :"A website for my friends cookie bussines that he sadly no longer runs(pixel art themed).", 
-            image :  "/lekhalilecookies.png",
-            status :  "I dont really know.",
-            link : "https://lekhalillecookies.netlify.app/",
-            github : "https://github.com/So8oS/le_khalil_le_cookies-V2"
-        },
-        {
-            id : 3,
             title :  "Meooww",
             description :" Just a cute page with cat pics that I made to practice tailwind css.",
             image :  "/meow.png",
@@ -38,7 +29,7 @@ export const projects = [
 
         },
         {
-            id : 4,
+            id : 3,
             title :  "My Unsplash",
             description :"This app allows you to store and access your favorite Unsplash pictures in one place.",
             image :  "/myunsplash.png",
@@ -47,7 +38,7 @@ export const projects = [
             github : "https://github.com/So8oS/so8os-unsplash"
         },
         {
-            id : 5,
+            id : 4,
             title :  "Cat wiki",
             description :"A website to browse cat breeds and know more about them.",
             image :  "/catwiki.png",
@@ -56,7 +47,7 @@ export const projects = [
             github : "https://github.com/So8oS/catwiki"
         },
         {
-            id : 6,
+            id : 5,
             title :  "SMDB",
             description :"A movie database where you can browse movies.",
             image :  "/smdb.png",
@@ -64,10 +55,16 @@ export const projects = [
             link : "https://smdb-so8os.vercel.app",
             github : "https://github.com/So8oS/smdb"
         },
+        {
+            id : 6,
+            title :  "Le Khalil Le Cookies",
+            description :"A website for my friends cookie bussines that he sadly no longer runs(pixel art themed).", 
+            image :  "/lekhalilecookies.png",
+            status :  "Almost Finished",
+            link : "",
+            github : "https://github.com/So8oS/le_khalil_le_cookies_v3"
+        },
 
 
        
       ];
-
-
-      //            image :  "https://images.unsplash.com/photo-1536566482680-fca31930a0bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
