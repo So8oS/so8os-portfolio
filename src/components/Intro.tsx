9d915d7b632558bacb52d622443cdf62f86b2134
So8oS <Ahmadothman033@gmail.com>
2023-07-01 00:16:26 +0400

    added prisma to stack + fixes

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,50 +1,55 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react'
-import Link from 'next/link'
-
+import React from "react";
+import Link from "next/link";
 
 const Intro = () => {
   return (
-    <div className='flex flex-col-reverse justify-center items-center mb-10 
-    md:flex-row gap-16 md:mb-24 '
-    data-aos="fade-up"
-     data-aos-offset="500"
-     data-aos-delay="50"
-     data-aos-duration="1000"
-     data-aos-easing=" ease-in-out"
-     data-aos-mirror="true"
-     data-aos-once="true"
-     data-aos-anchor-placement="top-bottom"
-     
+    <div
+      className="mb-10 flex flex-col-reverse items-center justify-center 
+    gap-16 md:mb-24 md:flex-row "
+      data-aos="fade-up"
+      data-aos-offset="500"
+      data-aos-delay="50"
+      data-aos-duration="1000"
+      data-aos-easing=" ease-in-out"
+      data-aos-mirror="true"
+      data-aos-once="true"
+      data-aos-anchor-placement="top-bottom"
     >
-        <div className='flex flex-col '>
+      <div className="flex flex-col ">
+        <span className=" text-[1.5rem] md:text-[1.75rem]">
+          {" "}
+          Helloo, I&apos;m Ahmad{" "}
+        </span>
+        <div className="flex animate-pulse flex-col">
+          <span className="text-6xl font-bold md:text-7xl  ">Full Stack</span>
+          <span className="text-6xl font-bold md:text-7xl  ">Developer</span>
+        </div>
 
-            <span className=' text-[1.5rem] md:text-[1.75rem]'> Helloo, I&apos;m Ahmad </span>
-            <div className='flex flex-col animate-pulse'>
-                <span className='text-6xl font-bold md:text-7xl  '>Full Stack</span>
-                <span className='text-6xl font-bold md:text-7xl  '>Developer</span>
-            </div> 
-            <span className=' animate-bounce text-[1.5rem] md:text-[1.75rem] '>woooooooo</span>
-
-            <Link href="/resume.pdf" download   >
-              <button
-              className=' w-full md:w-40 h-14 font-Poppins focus:outline-none  bg-darkYellow stripes border
-              hover:scale-110 transition-all duration-300 active:scale-75
-              border-darkBrown  text-darkBrown font- shadow-2xl rounded-xl mt-6
-              dark:stripes-dark dark:text-lightYellow
-              ' >Resume</button>
-            </Link>
-        
-        </div>     
-        <div className="  w-72 border-white md:w-[22rem] " style={{ clipPath: 
-          // 'polygon(50% 0%, 80% 10%, 100% 1%, 96% 70%, 80% 90%, 50% 100%, 20% 90%, 0% 70%, 0% 35%, 20% 10%)'
-          'polygon(50% 0, 80% 10%, 100% 0, 100% 75%, 50% 100%, 0 75%, 0% 25%, 0 0, 20% 10%)'
-          
-          }} >
-            <img className='' src="/pic.jpg" alt="ahmad" />
-        </div>   
+        <Link href="/resume.pdf" download>
+          <button
+            className=" stripes font- dark:stripes-dark mt-6 h-14  w-full rounded-xl border
+              border-darkBrown bg-darkYellow font-Poppins text-darkBrown
+              shadow-2xl  transition-all duration-300 hover:scale-110 focus:outline-none active:scale-75
+              dark:text-lightYellow md:w-40
+              "
+          >
+            Resume
+          </button>
+        </Link>
+      </div>
+      <div
+        className="  w-72 border-white md:w-[22rem] "
+        style={{
+          clipPath:
+            // 'polygon(50% 0%, 80% 10%, 100% 1%, 96% 70%, 80% 90%, 50% 100%, 20% 90%, 0% 70%, 0% 35%, 20% 10%)'
+            "polygon(50% 0, 80% 10%, 100% 0, 100% 75%, 50% 100%, 0 75%, 0% 25%, 0 0, 20% 10%)",
+        }}
+      >
+        <img className="" src="/pic.jpg" alt="ahmad" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -1,37 +1,49 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react'
+import React from "react";
 
 const Stack = () => {
-
-
-
   return (
-    <div className='flex flex-col justify-center items-center mb-16'
-    data-aos="fade-up"
-    data-aos-offset="200"
-    data-aos-delay="50"
-    data-aos-duration="1000"
-    data-aos-easing=" ease-in-out"
-    data-aos-mirror="true"
-    data-aos-once="false"
-    data-aos-anchor-placement="top-bottom"
+    <div
+      className="mb-16 flex flex-col items-center justify-center"
+      data-aos="fade-up"
+      data-aos-offset="200"
+      data-aos-delay="50"
+      data-aos-duration="1000"
+      data-aos-easing=" ease-in-out"
+      data-aos-mirror="true"
+      data-aos-once="false"
+      data-aos-anchor-placement="top-bottom"
     >
-        <span className='text-6xl font-bold md:text-7xl '>Tech</span>
-        {/* <div className='mt-16 grid grid-cols-2  justify-center items-center  gap-2 
+      <span className="text-6xl font-bold md:text-7xl ">Tech</span>
+      {/* <div className='mt-16 grid grid-cols-2  justify-center items-center  gap-2 
         md:flex-row md:grid-cols-4 content-center' > */}
-        <div className='mt-16 flex flex-wrap   justify-center items-center  gap-2 
-        md: ' >
-        
-            <img className=' w-32 md:w-44' src="/Icons/react.svg" alt="React JS" />
-            <img className=' w-32 md:w-44' src="/Icons/next.svg" alt="Next JS" />
-            <img className=' w-32 md:w-44' src="/Icons/css.svg" alt="CSS" />
-            <img className=' w-32 md:w-44' src="/Icons/tailwind.svg" alt="Tailwind CSS" />
-            <img className=' w-32 md:w-44' src="/Icons/javascript.svg" alt="Javascript" />
-            <img className=' w-32 md:w-44' src="/Icons/typescript.svg" alt="Typescript" />
-            <img className=' w-32 md:w-44' src="/Icons/csharp.svg" alt="C#" />
-        </div>
+      <div
+        className="md: mt-16 flex   flex-wrap items-center  justify-center 
+        gap-2 "
+      >
+        <img className=" w-32 md:w-44" src="/Icons/react.svg" alt="React JS" />
+        <img className=" w-32 md:w-44" src="/Icons/next.svg" alt="Next JS" />
+        <img className=" w-32 md:w-44" src="/Icons/css.svg" alt="CSS" />
+        <img
+          className=" w-32 md:w-44"
+          src="/Icons/tailwind.svg"
+          alt="Tailwind CSS"
+        />
+        <img
+          className=" w-32 md:w-44"
+          src="/Icons/javascript.svg"
+          alt="Javascript"
+        />
+        <img
+          className=" w-32 md:w-44"
+          src="/Icons/typescript.svg"
+          alt="Typescript"
+        />
+        <img className=" w-32 md:w-44" src="/Icons/prisma.svg" alt="prisma" />
+        <img className=" w-32 md:w-44" src="/Icons/csharp.svg" alt="C#" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Stack
+export default Stack;
