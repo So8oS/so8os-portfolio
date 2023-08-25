f72a1890438af4637ca5e76044d98523a430eaf1
So8oS <Ahmadothman033@gmail.com>
2023-08-25 15:43:37 +0400

    new projects added - condetional rendering for links

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,52 +1,61 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react'
-import { AiFillGithub } from 'react-icons/ai';
+import React from "react";
+import { AiFillGithub } from "react-icons/ai";
 
-interface projectCardProps{
+interface projectCardProps {
   name: string;
   image: string;
   description: string;
   status: string;
-  link : string;
-  github : string;
+  link: string;
+  github: string;
 }
 
-
-
-export const ProjectCard = ( props:projectCardProps) => {
+export const ProjectCard = (props: projectCardProps) => {
   return (
-    <div className=' flex flex-col  w-[20rem] justify-center items-center  h-[25rem] rounded-md gap-3 
-     md:w-[23rem]
-     md:hover:scale-110 transition-all duration-500 
-     '
-     data-aos="fade-up"
-     data-aos-offset="200"
-     data-aos-delay="50"
-     data-aos-duration="1100"
-     data-aos-easing="ease-in-out"
-     data-aos-mirror="false"
-     data-aos-once="false"
-     data-aos-anchor-placement="top-bottom"
-     >
-        <div>
-            <img className='rounded-md w-80 h-48' src={props.image} alt="" />
-        </div>
+    <div
+      className=" flex h-[25rem]  w-[20rem] flex-col items-center  justify-center gap-3 rounded-md 
+     transition-all
+     duration-500 md:w-[23rem] md:hover:scale-110 
+     "
+      data-aos="fade-up"
+      data-aos-offset="200"
+      data-aos-delay="50"
+      data-aos-duration="1100"
+      data-aos-easing="ease-in-out"
+      data-aos-mirror="false"
+      data-aos-once="false"
+      data-aos-anchor-placement="top-bottom"
+    >
+      <div>
+        <img className="h-48 w-80 rounded-md " src={props.image} alt="" />
+      </div>
 
-        <span className=''>{`Status: ${props.status}`}</span>
+      <span className="">{`Status: ${props.status}`}</span>
 
-        <div className='flex flex-col items-center text-center h-10 mb-12'>
-            <span className='font-bold text-lg '>{props.name}</span>
-            <span className='w-[20.2rem] '>{props.description}</span>
-        </div>
+      <div className="mb-12 flex h-10 flex-col items-center text-center">
+        <span className="text-lg font-bold ">{props.name}</span>
+        <span className="w-[20.2rem] ">{props.description}</span>
+      </div>
 
-        <div className='flex justify-center items-center gap-2'>
-          <a className='hover:text-gray-500 ' href={props.github} target='_blank' rel="noreferrer"><AiFillGithub className='cursor-pointer  ' /></a>
-          <span>|</span>
-          <a className='' href={props.link} target='_blank' rel="noreferrer" >
-          <span className='font-extrabold hover:text-gray-500' >browse</span>
-        </a>
-        </div>
-        
+      <div className="flex items-center justify-center gap-2">
+        {props.github && (
+          <a
+            className="hover:text-gray-500 "
+            href={props.github}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillGithub className="cursor-pointer  " />
+          </a>
+        )}
+        {props.github && props.link && <span>|</span>}
+        {props.link && (
+          <a className="" href={props.link} target="_blank" rel="noreferrer">
+            <span className="font-extrabold hover:text-gray-500">browse</span>
+          </a>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,70 +1,78 @@
 export const projects = [
-        {
-            id :  0,
-            title :  "Image Uploader",
-            description :"A tool for uploading and copying images to clipboard.",
-            image :  "/image-uploader.png",
-            status :  "Finished",
-            link : "https://so8os-image-uploader.vercel.app/",
-            github : "https://github.com/So8oS/image-uploader-next"
-        },
-        {
-            id : 1,
-            title :  "Quote Generator",
-            description :"Generates random quotes on button click.",
-            image :  "/quote-generator.png",
-            status :  "Finished",
-            link : "https://ahmadothman-quotegenerator.netlify.app/",
-            github : "https://github.com/So8oS/quotes"
-
-        },
-        {
-            id : 2,
-            title :  "Meooww",
-            description :" Just a cute page with cat pics that I made to practice tailwind css.",
-            image :  "/meow.png",
-            status :  "Finished",
-            link : "https://meooww.netlify.app/",
-            github : "https://github.com/So8oS/tailwind_practice_meoowww"
-
-        },
-        {
-            id : 3,
-            title :  "My Unsplash",
-            description :"This app allows you to store and access your favorite Unsplash pictures in one place.",
-            image :  "/myunsplash.png",
-            status :  "Finished",
-            link : "https://so8os-unsplash.vercel.app/",
-            github : "https://github.com/So8oS/so8os-unsplash"
-        },
-        {
-            id : 4,
-            title :  "Cat wiki",
-            description :"A website to browse cat breeds and know more about them.",
-            image :  "/catwiki.png",
-            status :  "Finished",
-            link : "https://catwiki-so8os.vercel.app/",
-            github : "https://github.com/So8oS/catwiki"
-        },
-        {
-            id : 5,
-            title :  "SMDB",
-            description :"A movie database where you can browse movies.",
-            image :  "/smdb.png",
-            status :  "Finished",
-            link : "https://smdb-so8os.vercel.app",
-            github : "https://github.com/So8oS/smdb"
-        },
-        {
-            id : 6,
-            title :  "Le Khalil Le Cookies",
-            description :"A website for my friends cookie bussines that he sadly no longer runs(pixel art themed).", 
-            image :  "/lekhalilecookies.png",
-            status :  "Almost Finished",
-            link : "",
-            github : "https://github.com/So8oS/le_khalil_le_cookies_v3"
-        },
-
-
-       
-      ];
+  {
+    id: 0,
+    title: "Image Uploader",
+    description: "A tool for uploading and copying images to clipboard.",
+    image: "/image-uploader.png",
+    status: "Finished",
+    link: "https://so8os-image-uploader.vercel.app/",
+    github: "https://github.com/So8oS/image-uploader-next",
+  },
+  {
+    id: 1,
+    title: "Quote Generator",
+    description: "Generates random quotes on button click.",
+    image: "/quote-generator.png",
+    status: "Finished",
+    link: "https://ahmadothman-quotegenerator.netlify.app/",
+    github: "https://github.com/So8oS/quotes",
+  },
+  {
+    id: 2,
+    title: "Meooww",
+    description:
+      " Just a cute page with cat pics that I made to practice tailwind css.",
+    image: "/meow.png",
+    status: "Finished",
+    link: "https://meooww.netlify.app/",
+    github: "https://github.com/So8oS/tailwind_practice_meoowww",
+  },
+  {
+    id: 3,
+    title: "My Unsplash",
+    description:
+      "This app allows you to store and access your favorite Unsplash pictures in one place.",
+    image: "/myunsplash.png",
+    status: "Finished",
+    link: "https://so8os-unsplash.vercel.app/",
+    github: "https://github.com/So8oS/so8os-unsplash",
+  },
+  {
+    id: 4,
+    title: "Cat wiki",
+    description: "A website to browse cat breeds and know more about them.",
+    image: "/catwiki.png",
+    status: "Finished",
+    link: "https://catwiki.ahmadothman.com/",
+    github: "https://github.com/So8oS/catwiki",
+  },
+  {
+    id: 5,
+    title: "SMDB",
+    description: "A movie database where you can browse movies.",
+    image: "/smdb.png",
+    status: "Finished",
+    link: "https://smdb.ahmadothman.com/",
+    github: "https://github.com/So8oS/smdb",
+  },
+  {
+    id: 6,
+    title: "Le Khalil Le Cookies",
+    description:
+      "A website for my friends cookie bussines that he sadly no longer runs(pixel art themed).",
+    image: "/lekhalilecookies.png",
+    status: "Finished",
+    link: "https://lekhalillecookies.ahmadothman.com/",
+    github: "https://github.com/So8oS/le_khalil_le_cookies_v3",
+  },
+  {
+    id: 7,
+    title: "Ancient Gamer",
+    description:
+      "A website for my social media/Youtube page Ancient Gamer which has the content a long games.",
+    image: "/ancientgamer.png",
+    status: "Finished",
+    link: "https://www.ancientgamer.net/",
+    github: "",
+  },
+];
